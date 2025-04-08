0e90c1d14c1a2a9cd27f9786c3f9d556dd64aaf2
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-08 16:25:49 +0500

    SDK-agent

---
+++ ai-agent/main.py
@@ -0,0 +1,17 @@
+import os 
+from dotenv import load_dotenv
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+
+load_dotenv()
+
+gemini_Api_key = os.getenv("GEMINI_API_KEY")
+
+provider = AsyncOpenAI(
+    api_key = gemini_Api_key
+    base_url = "https://generativelanguage.googleapis.com/v1beta/openai",
+),
+
+model = OpenAIChatCompletionsModel(
+    model = "gemini-2.0-flash"
+
+)
